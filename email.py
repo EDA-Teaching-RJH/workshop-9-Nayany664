8042b986418a978e0b0681ee0a7186bbc2a60194
Nayany664 <nm664@kent.ac.uk>
2024-11-27 11:27:38 +0000

    email - comments

--- email.py
+++ email.py
@@ -1,1 +1,12 @@
+import re
 
+email = input("Enter your email").strip()
+
+if re.search(r"^\w+@ac.uk$",email) :
+    print("Valid Academic Email")
+elif re.search(r"\w+@gov.uk$",email) :
+    print("Valid Government Email")
+elif re.search(r"\w+@nhs.net$",email) :
+    print("Valid NHS Email")
+else :
+    print("Invalid")
